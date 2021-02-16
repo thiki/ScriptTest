32b6191d85c4d571b71e2276661eac4d8bfeb117
Take <takehana@thikipedia.jp>
2021-02-16 16:47:58 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,36 +1,56 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class Boss {
+public class Boss
+{
     private int mp = 53;
 
     // 魔法攻撃用の関数
-    public void Magic(int mpconsump) {
-        if (mpconsump <= mp) {
+    public void Magic(int mpconsump) 
+    {
+        if (mpconsump <= mp) 
+        {
             // 残りmpを減らす
             this.mp -= mpconsump;
             Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
-        } else {
+        }
+        else
+        {
             Debug.Log("MPが足りないため魔法が使えない。");
         }
     }
 
 }
 
-public class Test : MonoBehaviour {
-    void Start() {
+public class Test : MonoBehaviour
+{
+    void Start() 
+    {
+        int[] array = {10, 20, 5, 40, 19};
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
 
         // Bossクラスの変数を宣言してインスタンスを代入
         Boss lastboss = new Boss();
 
         // 10回処理を繰り返す
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 11; i++)
+        {
             // 魔法用の関数を呼び出す
             lastboss.Magic(5);
         }
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+
     }
 }
